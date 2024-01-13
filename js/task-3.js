9af2448175bfb0d47f11ba718dc21eb0e031c88a
TamaraKarlova <tamara.karlova@gmail.com>
2024-01-13 10:43:25 +0100

    update task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,10 @@
 function getElementWidth(content, padding, border) {
-// const elementWidthWidth = 
-return `${Number.parseFloat(content) + Number.parseFloat(padding) + Number.parseFloat(padding) + Number.parseFloat(border) + Number.parseFloat(border)}`
-
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
+    return totalWidth;
 }
-
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
